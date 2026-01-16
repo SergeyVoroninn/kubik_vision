5693a375fdae0d4cafd056ff928f7ab8d8b15c74
Voronin S. <Serghey117@gmail.com>
2026-01-16 19:24:03 +0300

    remove_hard_mode

--- clear_hard.py
+++
@@ -1,5 +0,0 @@
-import os
-names_of_files = ['phase1x24_prun','move_twist','move_slice_sorted','move_flip','move_corners','fs24_sym','fs24_rep','fs24_classidx','cornerprun','conj_twist']
-for f in names_of_files:
-    if os.path.exists(f):
-        os.remove(f)
